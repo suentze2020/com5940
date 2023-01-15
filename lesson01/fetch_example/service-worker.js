9d31e2e5f14f2c6d87afecbae69fe292cb6b7191
suentze2020 <suentze2020@gmail.com>
2023-01-15 22:18:59 +0800

    Fetch Example PWA
    Fetch Example PWA

---
+++ lesson01/fetch_example/service-worker.js
@@ -0,0 +1,56 @@
+var CACHE_NAME = "v2";
+var urlsToCache = [
+  "/lesson01/fetch_example",
+  "/lesson01/fetch_example/lesson01.html",
+  "/lesson01/fetch_example/css/lesson01.css",
+  "/lesson01/fetch_example/js/lesson01.js",
+  "/lesson01/fetch_example/js/main.js",
+  "/lesson01/fetch_example/js/module.js",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+
+      var fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(function (response) {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    })
+  );
+});
